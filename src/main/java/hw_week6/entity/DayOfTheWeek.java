0e6f5619f381187b64bac96ab2ee39e80ab6bac9
Veronika Chupova <della85c@yahoo.com>
2024-06-11 12:53:24 +0200

    added enum as separate class

--- src/main/java/hw_week6/entity/DayOfTheWeek.java
+++ src/main/java/hw_week6/entity/DayOfTheWeek.java
@@ -2,12 +2,14 @@ package hw_week6.entity;
 
 import java.util.Scanner;
 
+import hw_week6.entity.MyEnum;
+
 public class DayOfTheWeek {
 
-    enum MyDay {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
+
 
     public static void main (String[] args) {
-        MyDay today = MyDay.MONDAY;
+        MyEnum.MyDay today = MyEnum.MyDay.MONDAY;
         switch (today) {
             case SATURDAY, SUNDAY:
                 System.out.println("Weekend");
---
+++ src/main/java/hw_week6/entity/MyEnum.java
@@ -0,0 +1,5 @@
+package hw_week6.entity;
+
+public class MyEnum {
+    enum MyDay {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
+}
